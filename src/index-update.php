8315cd96f18f59807e16a68794d473f01b5e4950
bhj017673 <baehyunjinny48@gmail.com>
2024-10-16 11:13:09 +0900

    1016 현진 index 수정완료

--- src/index-update.php
+++ src/index-update.php
@@ -37,7 +37,7 @@ try{
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Main-page</title>
-    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/index-update.css">
 </head>
 <body>
     <container>
