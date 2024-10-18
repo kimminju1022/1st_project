9252a1722e99c28f6c8a7f5986392cd03c21b812
kimminju1022 <nanum112@naver.com>
2024-10-18 10:00:00 +0900

    1017 원상 insert 경로 수정

--- src/todo_list_insert.php
+++ src/todo_list_insert.php
@@ -151,7 +151,7 @@
                         </div>
                         
                         <div class="btn-insert">
-                            <a href="/todo_list_detail.php?<?php echo "id=".$id."&page_todo=".$page_todo."&page_checklist=".$page_checklist ?>"><button type="button" class="btn">뒤로가기</button></a>
+                            <a href="/board.php?<?php echo "page_todo=".$page_todo."&page_checklist=".$page_checklist ?>"><button type="button" class="btn">뒤로가기</button></a>
                             <button type="submit" class="btn">작성 완료</button>
                             <input type="hidden" name="posttype" value="insert">
                         </div>
